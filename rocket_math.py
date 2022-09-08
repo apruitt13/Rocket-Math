24d3d2b4072ad5e43a3ffc81c76b9df0869bdda7
Adam Pruitt <pruittad@oregonstate.edu>
2022-09-08 13:39:05 -0700

    The ship moves up and down.

--- rocket_math.py
+++ rocket_math.py
@@ -68,6 +68,10 @@ class NumberInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
@@ -79,6 +83,11 @@ class NumberInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
